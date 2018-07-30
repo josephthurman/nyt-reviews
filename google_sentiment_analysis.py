011fbaf79739322caa5543e2aa4656f5ac447210
Joseph Thurman <joethurman3@gmail.com>
2018-07-30 19:14:46 -0400

    Redid sentiment analysis after stripping whitespace.  Added basic exploratory analysis and first model building

--- google_sentiment_analysis.py
+++ google_sentiment_analysis.py
@@ -2,6 +2,7 @@
 import json
 import config
 import copy
+import re
 
 # Imports the Google Cloud client library
 from google.cloud import language
@@ -14,19 +15,31 @@ client = language.LanguageServiceClient()
 with open('cleaned_review_data.json', 'r') as input:
     reviews = json.load(input)
 
-# Pick an arbitrary review to check
-test_text = reviews[115].get('review_text')
 
 # Find sentiment in each text via Google
 analyzed_reviews = []
 for review in reviews:
-    document = types.Document(content=review.get('review_text'), type=enums.Document.Type.PLAIN_TEXT)
+    text1 = review.get('review_text')
+    text2 = re.sub(r'\s+', ' ', text1)
+
+    # With extraneous whitespace
+    document = types.Document(content=text1, type=enums.Document.Type.PLAIN_TEXT)
     sentiment = client.analyze_sentiment(document=document).document_sentiment
-    google_score = sentiment.score
-    google_mag = sentiment.magnitude
+    google_score1 = sentiment.score
+    google_mag1 = sentiment.magnitude
+
+    # Without - we want this one, seems to give a little more variation
+    document = types.Document(content=text2, type=enums.Document.Type.PLAIN_TEXT)
+    sentiment = client.analyze_sentiment(document=document).document_sentiment
+    google_score2 = sentiment.score
+    google_mag2 = sentiment.magnitude
+
+    # Save
     new_review = copy.deepcopy(review)
-    new_review['google_score'] = google_score
-    new_review['google_mag'] = google_mag
+    new_review['google_score1'] = google_score1
+    new_review['google_mag1'] = google_mag1
+    new_review['google_score2'] = google_score2
+    new_review['google_mag2'] = google_mag2
     analyzed_reviews.append(new_review)
 
 with open('analyzed_review_data.json', 'w') as outfile:
