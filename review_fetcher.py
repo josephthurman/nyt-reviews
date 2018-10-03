15067020ecb96acfc89a2b14c38ebd3e0dbf7841
Joseph Thurman <joethurman3@gmail.com>
2018-10-03 15:17:44 -0400

    Remove HTML data, update review_fetcher script

--- review_fetcher.py
+++ review_fetcher.py
@@ -15,18 +15,18 @@ def get_urls():
     Returns list of the URLs, or an empty list if there was a problem
     """
     query_url = 'http://api.nytimes.com/svc/search/v2/articlesearch.json' + \
-                '?api_key=' + config.NYT_API_KEY + '&begin_date=20130101' + '&fl=web_url' + \
+                '?api_key=' + config.NYT_API_KEY + '&begin_date=20130101' + '&end_date = 20181003'+ '&fl=web_url' + \
                 '&fq=byline:("Pete Wells")ANDtype_of_material:("Review")ANDnews_desk:("Dining","Food")'
     # Note API key imported from config file to avoid putting confidential stuff on github
     # Query format according to documentation available from NYT
 
-    # Make a first query just to get the number of responses returned.
-    # Only get responses in pages of 10, so we need to know how many pages to loop through
+    # Make a first query just to get the total number of reviews
     first_query = requests.get(query_url).json()
     hits = first_query.get('response').get('meta').get('hits')
     num_pages = hits // 10
 
     # Fetch all the urls
+    # Are limited to fetching in batches of 10
     returned_url_list = []
     for page in range(num_pages + 1):
         time.sleep(1)  # Can't make too many requests per second or the API gets mad
@@ -39,9 +39,7 @@ def get_urls():
                 returned_url_list.append(url)
 
     if len(returned_url_list) == hits:
-        # After inspection, this still contains some articles we don't want - book reviews written by Pete Wells,
-        # some misclassified recipes, special content related to a review that's not actually the review itself, etc.
-        # We can find some of these by inspecting the url, and we remove those now.
+        # Remove some non-review articles returned by the API
         bad_words = ["(blog)", "(interactive)", "(wine-school)", "(insider)", "(hungry-city)", "(best)",
              "(/books/)", "(slideshow)", "(obituaries)", "(recipes)", "(reader-center)", "(technology)"]
         final_url_list = []
@@ -69,44 +67,26 @@ def is_misclassified(bs):
     return(False)
 
 
-def get_reviews(urls, n=10):
+def get_reviews(to_fetch, n=10):
     """
     Downloads the reviews from NYT web page.
 
-    urls is a list of the urls to retrieve. N is the max number of times to try to download a page
+    urls is a list of the urls to retrieve. n is the max number of times to try to download a page
 
     Downloads each page and saves it as html. Also saves a list of the urls of
     every page. Does some parsing of the pages to remove non-review urls that still haven't been caught
     """
-    refetch = []
     final_url_list = []
     counter = 0
     os.makedirs('reviews', exist_ok=True)
-    # First loop - download HTML for each url, check that it's valid, and save it if so
-    for review_url in urls:
-        review = requests.get(review_url)
-        parsed_review = BeautifulSoup(review.content, 'html.parser')
-        if find_server_error(parsed_review):
-            refetch.append(review_url)
-        elif is_misclassified(parsed_review):
-            continue
-        else:
-            with open('./reviews/review' + str(counter) + '.html', 'w') as newfile:
-                newfile.write(parsed_review.prettify())
-            final_url_list.append(review_url)
-            counter += 1
 
-    # Retry on any pages that didn't download properly
-    missing = len(refetch)
-    attempts = 0
-    while (missing > 0) and (attempts <= n):
-        still_refetch = []
-        for review_url in refetch:
-            print("Trying again for URL: " + review_url)
+    def get_from_list(url_list, final_url_list, counter):
+        refetch_list = []
+        for review_url in url_list:
             review = requests.get(review_url)
             parsed_review = BeautifulSoup(review.content, 'html.parser')
             if find_server_error(parsed_review):
-                still_refetch.append(review_url)
+                refetch_list.append(review_url)
             elif is_misclassified(parsed_review):
                 continue
             else:
@@ -114,15 +94,21 @@ def get_reviews(urls, n=10):
                     newfile.write(parsed_review.prettify())
                 final_url_list.append(review_url)
                 counter += 1
-        missing = len(still_refetch)
+        return (refetch_list, final_url_list, counter)
 
-    if missing > 0:
+    attempts = 0
+    while (len(to_fetch) > 0) and (attempts <= n):
+        to_fetch, final_url_list, counter = get_from_list(to_fetch, final_url_list, counter)
+        attempts += 1
+
+    if len(to_fetch) > 0:
         print("Could not successfully access the following reviews:")
-        for url in still_refetch:
+        for url in to_fetch:
             print(url)
 
     with open('./reviews/url_list.txt', 'w') as url_output:
         json.dump(final_url_list, url_output)
+
 
 if __name__ == '__main__':
     urls = get_urls()
